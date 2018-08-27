1cc682b492d114fcc94004e91ec26dc3e6164b5d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-27 17:53:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id] 
-    @artist = Artist.find(params[:artist_id])
+    @artist = Artist.find_by_id(params[:artist_id])
       if @artist.nil?
         redirect_to artists_path
       end
