904db8de0fd4642e9c2c1d27eae946c15d758b10
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-27 17:14:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :artists, only: [:show] do
     resources :songs, only: [:show, :index]
   end
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+    root 'songs#index'
 end
